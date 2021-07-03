1c983caf69aa0986f082cfe9a069645707b7103a
ashrafuddin1496 <asraf1496@gmail.com>
2021-07-03 07:58:05 +0600

    created header footer php and registered nav menu

--- functions.php
+++ functions.php
@@ -11,11 +11,13 @@ function philosophy_after_setup(){
 	add_theme_support('html5',array('search-form','comment-list'));
 	add_theme_support('post-formats',array('image','gallery','quote','audio','video','link'));
 	add_editor_style('/assets/css/editor-style.css');
+	//Register Menu
+	register_nav_menu( 'topmenu',__('Top Menu','philosophy'));
 }
 add_action('after_setup_theme','philosophy_after_setup');
 function philosophy_assets(){
 	//stylesheets
-	wp_enqueue_style( 'fontaweseom-css',get_theme_file_uri('/assets/css/font-awesome/css/font-awesome.css'),null,VERSION));
+	wp_enqueue_style( 'fontaweseom-css',get_theme_file_uri('/assets/css/font-awesome/css/font-awesome.css'),null,VERSION);
 	wp_enqueue_style( 'fonts-css',get_theme_file_uri('/assets/css/fonts.css'),null,1.0);
 	wp_enqueue_style( 'base-css',get_theme_file_uri('/assets/css/base.css'),null,1.0);
 	wp_enqueue_style( 'vendor-css',get_theme_file_uri('/assets/css/vendor.css'),null,1.0);
