5730e099666be00b573c95755dee9901d250e05b
ashrafuddin1496 <asraf1496@gmail.com>
2021-07-15 20:34:34 +0600

    contact page done

--- functions.php
+++ functions.php
@@ -62,12 +62,30 @@ remove_action( 'term_description','wpautop');
  */
 function philosophy_widgets() {
 	register_sidebar( array(
-		'name'          => __( 'About Us Page', 'textdomain' ),
+		'name'          => __( 'About Us Page', 'philosophy' ),
 		'id'            => 'about-us',
 		'description'   => __( 'Widgets in this area will be shown on about us page.', 'philosophy' ),
 		'before_widget' => '<div id="%1$s" class="col-block %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3 class="quarter-top-margin">',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'name'          => __( 'Contact Page Map Section', 'philosophy' ),
+		'id'            => 'contact-map',
+		'description'   => __( 'Widgets in this area will be shown on contact page.', 'philosophy' ),
+		'before_widget' => '<div id="%1$s" class="%2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '',
+		'after_title'   => '',
+	) );
+	register_sidebar( array(
+		'name'          => __( 'Contact Page Information Section', 'philosophy' ),
+		'id'            => 'contact-info',
+		'description'   => __( 'Widgets in this area will be shown on contact page.', 'philosophy' ),
+		'before_widget' => '<div id="%1$s" class="col-six tab-full %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="">',
 		'after_title'   => '</h3>',
 	) );
 }
