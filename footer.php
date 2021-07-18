5e1512358c6fca8fef8e4d4e96bb1a0556b4987e
ashrafuddin1496 <asraf1496@gmail.com>
2021-07-18 12:46:13 +0600

    footer tag list part done

--- footer.php
+++ footer.php
@@ -31,6 +31,7 @@
                     </article>
                 <?php
                 }
+                wp_reset_query();
                 ?>
 			</div> <!-- end popular_posts -->
 		</div> <!-- end popular -->
@@ -67,17 +68,15 @@
 			<h3>Tags</h3>
 
 			<div class="tagcloud">
-				<a href="#0">Salad</a>
-				<a href="#0">Recipe</a>
-				<a href="#0">Places</a>
-				<a href="#0">Tips</a>
-				<a href="#0">Friends</a>
-				<a href="#0">Travel</a>
-				<a href="#0">Exercise</a>
-				<a href="#0">Reading</a>
-				<a href="#0">Running</a>
-				<a href="#0">Self-Help</a>
-				<a href="#0">Vacation</a>
+					<?php
+					$tags = get_tags();
+					foreach ( $tags as $tag ) :
+						$tag_link = get_tag_link( $tag->term_id );
+						?>
+                            <a href='<?php echo $tag_link; ?>' title='<?php echo $tag->name; ?>' class='<?php echo $tag->slug ?>'><?php echo $tag->name ?></a>
+					<?php
+					endforeach;
+					?>
 			</div> <!-- end tagcloud -->
 		</div> <!-- end tags -->
 	</div> <!-- end tags-wrap -->
