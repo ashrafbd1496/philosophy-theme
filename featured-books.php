0a36f5b7bc9ef420c8a9fa22a16d1d5b1495f9e8
ashrafuddin1496 <asraf1496@gmail.com>
2021-08-04 18:28:44 +0600

    metabox bind in custom post and run custom meta query

---
+++ featured-books.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* Template Name: Featured Books
+*/
+?>
+<?php get_header(); ?>
+<!-- s-content
+================================================== -->
+<section class="s-content">
+
+	<div class="row masonry-wrap">
+		<div class="masonry">
+
+			<div class="grid-sizer"></div>
+
+            <?php
+            $arguments = array(
+                    'post_type' => 'book',
+                    'meta_key' => 'is_featured',
+                    'meta_value' => true,
+                    'post_per_page' => 3,
+            );
+            $books = NEW WP_Query($arguments);
+            while($books->have_posts()){
+	            $books->the_post();
+                get_template_part("template-parts/post-formats/post",get_post_format());
+            }
+            ?>
+
+
+		</div> <!-- end masonry -->
+	</div> <!-- end masonry-wrap -->
+
+	<div class="row">
+		<div class="col-full">
+			<nav class="pgn">
+				<?php philosophy_pagination(); ?>
+			</nav>
+		</div>
+	</div>
+
+</section> <!-- s-content -->
+
+<?php get_footer(); ?>
