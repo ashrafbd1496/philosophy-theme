880dce8f13b8d5665f37a89491a40885c574af92
ashrafuddin1496 <asraf1496@gmail.com>
2021-08-09 12:39:06 +0600

    taxonomy query done

---
+++ taxquery.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Tax Query Example
+ */
+
+$philosophy_query_args = array(
+	'post_type'=>'book',
+	'post_per_page'=>-1,
+	'tax_query'=>array(
+		array(
+			'taxonomy'=>'language',
+			'field'=>'slug',
+			'terms'=>['english','bangla','arabic'],
+		)
+	),
+);
+
+$philosophy_query_posts = New WP_Query($philosophy_query_args);
+while ($philosophy_query_posts->have_posts()){
+	$philosophy_query_posts->the_post();
+	the_title();
+	echo '<br/>';
+}
+wp_reset_query();
